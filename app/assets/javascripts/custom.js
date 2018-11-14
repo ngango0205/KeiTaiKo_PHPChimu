7bcc2fa0ad70eb8eee77d8088399c6de7bec506e
hoangtuyb96 <hoangtuyb96@gmail.com>
2018-11-14 09:56:25 +0700

    fix_menu_comment

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -19,23 +19,14 @@ $(window).on('load', function () {
 	});
 
 	showPopupBought();
-});
 
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
+  $('.hihi').on('click', function(){
+    var drop = $(this).parent().find('.dropdown-content');
+    if (drop.is(":hidden")) {
+    $('.dropdown-content').hide();
+      drop.show();
+    } else {
+      drop.hide();
     }
-  }
-}
+  })
+});
